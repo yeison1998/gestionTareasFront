53e7ec33d55e355d8176855f01f5813969a8d4ef
yeison6 <yeison@innclod.com>
2025-03-15 14:40:45 -0500

    cambiar url de la peticion

--- src/app/components/services/task-management.service.ts
+++ src/app/components/services/task-management.service.ts
@@ -11,18 +11,18 @@ export class TaskManagementService {
   http = inject(HttpClient);
 
   getTasks(): Observable<TaskManagement[]> {
-    return this.http.get<TaskManagement[]>(`https://localhost:7213/api/Tasks`);
+    return this.http.get<TaskManagement[]>(`https://apitareas-caf7b7fqgmg6dwf3.canadacentral-01.azurewebsites.net/api/Tasks`);
   }
 
   saveTask(task: TaskManagement): Observable<any> {
-    return this.http.post<any>(`https://localhost:7213/api/Tasks`, task);
+    return this.http.post<any>(`https://apitareas-caf7b7fqgmg6dwf3.canadacentral-01.azurewebsites.net/api/Tasks`, task);
   }
 
   updateTask(task: TaskManagement): Observable<{message: string, data: TaskManagement}> {
-    return this.http.put<{message: string, data: TaskManagement}>(`https://localhost:7213/api/Tasks/${task.id}`, task);
+    return this.http.put<{message: string, data: TaskManagement}>(`https://apitareas-caf7b7fqgmg6dwf3.canadacentral-01.azurewebsites.net/api/Tasks/${task.id}`, task);
   }
 
   deleteTask(id: number): Observable<any> {
-    return this.http.delete<any>(`https://localhost:7213/api/Tasks/${id}`);
+    return this.http.delete<any>(`https://apitareas-caf7b7fqgmg6dwf3.canadacentral-01.azurewebsites.net/api/Tasks/${id}`);
   }
 }
